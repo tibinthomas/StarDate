7b4afd1ed3ca487db19901eda8637f3515e6c03e
Tibin Thomas <Tibin Thomas>
2017-03-10 08:54:01 +0530

    Add project files.

---
+++ StarDate/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using StarDate.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarDate.Models
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        
+    }
+}
